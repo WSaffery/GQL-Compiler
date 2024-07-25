3d4d7dc81ec7226697de1048a44dc44aa30fb338
WSaffery <William.Saffery@gmail.com>
2024-07-25 12:54:47 +1000

    clear out old app files

--- src/main/java/TestRemoteDbApp.java
+++ src/main/java/TestRemoteDbApp.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -8,6 +6,10 @@ import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+
+import cli.Arg;
+import cli.CliArgParser;
+
 import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
 import graphs.GremlinGraph;
 import graphs.GremlinGraphFactory;
@@ -18,74 +20,76 @@ import graphs.ResourcePaths;
 // for some reason can't get remote-graph.properties files working
 // have to instantiate connection directly using remote-objects.yaml files
 
+// runs the load-drop test on every pair of given configs and graphs
+// batch version of RemoteDbApp for test mode
 public class TestRemoteDbApp {
-    // usage: -mode mode [-customKeys true/false] -confs conf1 conf2 ... -graphs graph1 graph2 ...
-    // where mode is either: print, load, or test
-    // customKeys is false by default, and should be false if the database doesn't support custom keys (i.e. Janus) 
-    // where conf_i is cluster yaml config files, ala remote-objects.yaml
-    // and graph_j is graph names
+    // usage: -customKeys val1 val2 ... -confs conf1 conf2 ... -graph graph1 graph2 ...
+    // customKeys is "false" by default, and should be false if the database doesn't support custom keys (i.e. Janus) 
+    // setting a single value of customKeys broadcasts to all configs, otherwise they are applied pairwise 
+    // confs are cluster yaml config files, ala remote-objects.yaml
+    // and graphs are graph names
+    // default config file is just "conf/remote-objects.yaml", default graph is just "g"
 
-    public static final String defaultMode = "test";
-    public static final boolean defaultCustomKeys = false;
+    public static final String defaultCustomKeys = "false";
     public static final String defaultConfigFile = "conf/remote-objects.yaml";
     public static final String defaultGraph = "g";
 
+    public static final CliArgParser argParser = new CliArgParser(Map.of(
+        "customKeys", Arg.multi(defaultCustomKeys),
+        "conf", Arg.multi(defaultConfigFile),
+        "graphs", Arg.multi(defaultGraph)
+    ));
+
     public static void main(String[] args) throws Exception {
-        Map<String, List<String>> map = argsSplit(args);
+        argParser.parseArgs(args);
 
-        String mode = map.containsKey("mode") ? map.get("mode").get(0) : defaultMode;
-        boolean customKeys = map.containsKey("customKeys") ? map.get("customKeys").get(0).equals("true") : defaultCustomKeys;
-        List<String> confs = map.containsKey("confs") ? map.get("confs") : List.of(defaultConfigFile);
-        List<String> graphs = map.containsKey("graphs") ? map.get("graphs") : List.of(defaultGraph);
+        List<String> confs = argParser.getArgs("conf");
+        List<String> graphs = argParser.getArgs("graphs");
+        List<String> customKeys = argParser.getArgs("customKeys");
 
-        System.out.println("confs: " + confs);
+        System.out.println("conf: " + confs);
         System.out.println("graphs: " + graphs);
 
-        for (String conf : confs)
+        if (customKeys.size() == 1 && confs.size() > 1)
         {
+            String base = customKeys.get(0);
+            for (int i = 0; i < confs.size() - 1; i++)
+            {
+                customKeys.add(base);
+            }
+        }
+
+        for (int i = 0; i < confs.size(); i++)
+        {
+            String conf = confs.get(i);
+            boolean customKeyConf = Boolean.valueOf(customKeys.get(i));
             DriverRemoteConnection connection = DriverRemoteConnection.using(
                 Cluster.open(conf), "g"
-             );
-
+                );
+            
             GraphTraversalSource gts = traversal().withRemote(connection);
             
-            // graph independent operation modes
-            if (mode.equals("print"))
+            System.out.println("Clearing db with config: " + conf);
+            dropGraph(gts);
+            System.out.println("Showing Clear Result");
+            showGraph(gts);
+
+            for (String graph : graphs)
             {
-                System.out.println("Showing graph with config: " + conf);
+                System.out.println("Loading graph " + graph + " with config: " + conf);
+                loadGraph(gts, graph, customKeyConf);
+                System.out.println("Showing Load Result");
                 showGraph(gts);
-            }
-            else if (mode.equals("drop"))
-            {
-                System.out.println("Dropping graph with config: " + conf);
+                System.out.println("Clearing graph");
                 dropGraph(gts);
+                System.out.println("Showing Clear Result");
+                showGraph(gts);
+
+                System.out.println("Finished for graph " + graph + " with config: " + conf);
             }
-            else 
-            {
-                for (String graph : graphs) // graph dependent operation modes
-                {
-                    System.out.println();
-                    if (mode.equals("test"))
-                    {
-                        System.out.println("Loading graph " + graph + " with config: " + conf);
-                        loadGraph(gts, graph, customKeys);
-                        System.out.println("Showing Load Result");
-                        showGraph(gts);
-                        System.out.println("Clearing graph");
-                        dropGraph(gts);
-                        System.out.println("Showing Clear Result");
-                        showGraph(gts);
-                    }
-                    else if (mode.equals("load"))
-                    {
-                        System.out.println("Loading graph " + graph + " with config: " + conf);
-                        loadGraph(gts, graph, customKeys);
-                    }
-                    
-                    System.out.println("Finished for graph " + graph + " with config: " + conf);
-                }
-            }
+
             gts.close();
+
         }
         
         // for some reason the program always hangs at the end, (it passes gts.close and all actual code)
@@ -94,27 +98,6 @@ public class TestRemoteDbApp {
         // which seems to not happen on close like it should
     }
 
-    public static Map<String, List<String>> argsSplit(String[] args)
-    {
-        Map<String, List<String>> map = new HashMap<>();
-        List<String> currentList = null;
-
-        for (String arg : args)
-        {
-            if (arg.charAt(0) == '-')
-            {
-                String key = arg.charAt(1) == '-' ? arg.substring(2) : arg.substring(1);
-                currentList = new ArrayList<>();
-                map.put(key, currentList);
-            }
-            else if (currentList != null)
-            {
-                currentList.add(arg);
-            }
-        }
-
-        return map;
-    }
 
     static void testGraph(GraphTraversalSource gts, String configFileName, String graphName) throws Exception 
     {
